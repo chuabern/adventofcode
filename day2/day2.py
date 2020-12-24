927e9b96e4c951b2d1bf996309ecaa3ed5512ce6
bernchua <34312496+bernchua@users.noreply.github.com>
2020-12-24 20:04:55 +0800

    day2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -26,3 +26,35 @@ def validate_passwords(passwords):
     return valid,invalid
 
 validate_passwords(passwords)
+
+def part2_passwords(passwords):
+    validity = 0
+
+    for i in passwords:
+        split = i.split(":")
+        policy = split[0].partition(" ")[0]
+        character = split[0].partition(" ")[2]
+        password = split[1][1:]
+
+        first_position = int(policy.partition("-")[0]) - 1
+        # first_position = int(first_position)-1
+        second_position = int(policy.partition("-")[2]) - 1
+
+        first_position_match = 0 
+        second_position_match = 0
+
+        if password[first_position] == character:
+            first_position_match += 1
+
+        if password[second_position] == character:
+            second_position_match += 1
+
+        matches = first_position_match + second_position_match
+
+        if matches == 1:
+            validity += 1
+
+    print("The number of valid passwords are {}".format(validity))
+    return validity
+
+part2_passwords(passwords)
