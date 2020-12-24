e5613f2196623cd063b5c455d5f7c09e6224009f
bernchua <34312496+bernchua@users.noreply.github.com>
2020-12-24 15:45:47 +0800

    day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -2,7 +2,7 @@
 with open("day1_input.txt") as f:
     numbers = list(f.read().split('\n'))
 
-def find_numbers(numbers):
+def find_two_numbers(numbers):
     for i in numbers:
         number_1 = int(i)
         for n in range(1,len(numbers)):
@@ -10,7 +10,18 @@ def find_numbers(numbers):
             sum = number_1 + number_2
             if sum == 2020:
                 return (number_1 * number_2)
-        if sum == 2020:
-            break
 
-find_numbers(numbers)
+print(find_two_numbers(numbers))
+
+def find_three_numbers(numbers):
+    for i in numbers:
+        number_1 = int(i)
+        for j in range(1,len(numbers)):
+            number_2 = int(numbers[j])
+            for k in range(2, len(numbers)):
+                number_3 = int(numbers[k])
+                sum = number_1 + number_2 + number_3
+                if sum == 2020:
+                    return (number_1 * number_2 * number_3)
+
+print(find_three_numbers(numbers))
